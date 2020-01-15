acd52d2da59408fe941474633f2c4d81509148e5
Andrew Scholly <SATmaster728@gmail.com>
2020-01-15 15:27:36 -0500

    first file after issue fix

---
+++ ex26.py
@@ -0,0 +1,74 @@
+print("You enter a dark room with three floors, such a bore.")
+print("1. floor 1")
+print("2. floor 2")
+print("3. floor 3")
+
+floor = input("> ")
+steak = 0
+if floor == "1":
+    print("You see a giant cake eating a steak")
+    print("what do you do not do?")
+    print("1. Take the steak, eat the cake")
+    print("2. Make loud noises, while hearing voices")
+    print("Or choose your own adventure you coporate zombie")
+
+    cake = input("> ")
+
+    if cake = "2":
+        print("The cake flops on you and you die from insanity. Good Job!")
+    elif cake = "1":
+        print("You become a normal american.")
+         print("Gain key item, steak")
+        print("You continue on, through winding passages and slowly become hungry")
+        print("1. Eat the steak")
+        print("2. Keep going")
+
+        eat = input("> ")
+
+        if steak = 0:
+            print("""Somehow you broke the game. Good job, you're now being digested
+             in the bowels of the zachulu. Please report this bug and we will get
+             todd howard to spend a few years hyping it's sixteen times fix.""")
+
+        elif steak = 1 and eat = "1":
+            print("You feel reinvigorted and keep going")
+            print("PLACEHOLDER")
+
+        elif steak = 1 and eat = "2":
+            print("You suffer from starvation and die. Good job!")
+    else:
+        print(f"Well, {bear} was perhaps better.")
+
+        steak = 0
+        print("You continue on, through winding passages and slowly become hungry")
+        print("1. Look around for something to eat")
+        print("2. Keep going")
+
+        food = input("> ")
+
+        if food = "1":
+            print("You find a dead body.")
+            deadbody = 1
+            print("1. Eat the flesh of the forgone")
+            print("2. Look for something else")
+
+            eat = input("> ")
+
+            if eat = "1":
+                print("You get E-coli from the food and die slowly. You probally should have cooked it first.")
+            elif eat = "2":
+                print("You find a stove, an electric one that works.")
+
+                stove = input("> ")
+                print("PLACEHOLDER")
+
+        elif food = "2":
+            print("You meet with a terrible fate and die. Good job!")
+elif floor == "2":
+    print("Placeholder.")
+
+elif floor == "3":
+    print("Placeholder.")
+
+else:
+    print("You phase through the ground and see the map. You're going to have a bad time")
